1ef35b2c54b96d61d0e5a0e8a48169f832f169ab
BLOOD212 <g95829914@gmail.com>
2026-04-08 20:17:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -15,6 +15,7 @@ global.owner = [
   ['212778494602', '𝕯𝖊ⱥ𝖉𝖑𝐲', true],
   ['212781816909', 'Luxifer', true],
   ['639350468907', 'bonzino', true],
+  ['393701330693', 'Blood', true],
 ]
 global.mods = ['xxxxxxxxxx', 'xxxxxxxxxx']
 global.prems = ['xxxxxxxxxx', 'xxxxxxxxxx']
